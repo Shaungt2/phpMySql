323f0743d068e9f31f0f5a2c349d95f675166238
student <student@iMac-7.local>
2018-04-23 16:19:30 -0400

    This is an other comment

--- config.php
+++ config.php
@@ -21,4 +21,5 @@ $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error() . "<br>");
 }
+//Hello this is a Comment
 ?>
