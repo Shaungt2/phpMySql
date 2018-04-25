b500d68f3d82db624d1c38f83efd029ae14ac0e3
Scott Horn <shorn813@outlook.com>
2018-04-24 22:16:22 -0400

    minor changes

--- standardDeviation.php
+++ standardDeviation.php
@@ -4,11 +4,11 @@ include_once("config.php");
 
 session_unset();
 
-// Select lowest score from scores table.
-$sql = "SELECT * FROM scores WHERE score=(select STDEV(score) from scores)";
+// Select standard deviation score from scores table.
+$sql = "SELECT * FROM scores WHERE score=(select STDEV (score) from scores)";
 $result = mysqli_query($conn, $sql);
 
-// Create a session variable, ls, to store the lowest score.
+// Create a session variable, sd, to store the lowest score.
 if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
     $_SESSION ["sd"] = $row["score"];   // Set hs session variable to low score in db.
