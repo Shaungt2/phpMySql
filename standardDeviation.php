b2480ad4f8f894c0ea68e5beb765c859d2032feb
Scott Horn <shorn813@outlook.com>
2018-04-24 22:44:45 -0400

    testing

--- standardDeviation.php
+++ standardDeviation.php
@@ -8,14 +8,16 @@ session_unset();
 //$sql = "SELECT * FROM scores WHERE score=(select STD(score) from scores)";
 $sql = "SELECT STD(score) FROM scores";
 $result = mysqli_query($conn, $sql);
+var_dump($result);
 
 // Create a session variable, sd, to store the lowest score.
 if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
-    $_SESSION ["sd"] = $row["score"];   // Set hs session variable to low score in db.
+    var_dump($row);
+    $_SESSION ["sd"] = $row["score"];   // Set sd session variable to low score in db.
 } else {
     session_unset();    // Remove all session variables.
 }
-echo "is this working";
+
 header('Location:scores.php');    // Go back to original page.
 ?>
