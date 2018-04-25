a04a7b7970b48e32f74290e6d6541e0db49de115
Scott Horn <shorn813@outlook.com>
2018-04-24 22:52:01 -0400

    testing

--- standardDeviation.php
+++ standardDeviation.php
@@ -11,6 +11,7 @@ $result = mysqli_query($conn, $sql);
 // Create a session variable, sd, to store the standard deviation.
 if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
+    var_dump($row);
     $_SESSION ["sd"] = $row["STD(score)"];
 } else {
     session_unset();    // Remove all session variables.
